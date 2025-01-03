51f4fe71f1fe35e3d314d5c7a071238b050b5334
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-01-03 19:42:14 +0500

    implent logic to prompt user for size

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const containerDiv = document.getElementById('container');
-const grid = () => {
+const grid = (m) => {
         const innerContainerDiv = document.createElement('div');
-        innerContainerDiv.className = 'container row';
+        innerContainerDiv.className = 'innerContainer row';
         containerDiv.appendChild(innerContainerDiv);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < m; j++) {
             const newDivC = document.createElement('div');
             newDivC.className = 'col square';
             innerContainerDiv.appendChild(newDivC);
@@ -12,7 +12,26 @@ const grid = () => {
             });            
     }
 }
-for (let i = 0; i < 16; i++) {
-    grid()
-}
 
+const makeGrid = (n = 16, m = n) => {
+    for (let i = 0; i < n; i++) {
+        grid(m)
+    }
+}
+makeGrid();
+const reset = () => {
+    containerDiv.innerHTML = '';
+}
+let size = 0;
+const getSize = () => {
+    let s = prompt(("Enter value (1-100)"), 16);
+    size = s;
+}
+const gridButton = document.getElementById('makeButton');
+gridButton.addEventListener('click', () => {
+    reset();
+    do {
+        getSize();
+    } while (size < 1 || size > 100);
+    makeGrid(size);
+});
